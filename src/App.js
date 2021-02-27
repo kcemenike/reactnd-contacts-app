c8de485155da544ca9a9d3a4a5cee2ddd3ef1761
kcemenike <29731161+kcemenike@users.noreply.github.com>
2021-02-27 06:06:07 +0100

    introducing the concept of state

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
 import React, { Component } from "react";
 import ListContacts from "./ListContacts";
 
-const contacts = [
-  {
-    id: "karen",
-    name: "Karen Isgrigg",
-    handle: "karen_isgrigg",
-    avatarURL: "http://localhost:5001/karen.jpg",
-  },
-  {
-    id: "richard",
-    name: "Richard Kalehoff",
-    handle: "richardkalehoff",
-    avatarURL: "http://localhost:5001/richard.jpg",
-  },
-  {
-    id: "tyler",
-    name: "Tyler McGinnis",
-    handle: "tylermcginnis",
-    avatarURL: "http://localhost:5001/tyler.jpg",
-  },
-];
-
 class App extends Component {
+  state = {
+    contacts: [
+      {
+        id: "karen",
+        name: "Karen Isgrigg",
+        handle: "karen_isgrigg",
+        avatarURL: "http://localhost:5001/karen.jpg",
+      },
+      {
+        id: "richard",
+        name: "Richard Kalehoff",
+        handle: "richardkalehoff",
+        avatarURL: "http://localhost:5001/richard.jpg",
+      },
+      {
+        id: "tyler",
+        name: "Tyler McGinnis",
+        handle: "tylermcginnis",
+        avatarURL: "http://localhost:5001/tyler.jpg",
+      },
+    ],
+  };
   render() {
-    return <ListContacts contacts={contacts} />;
+    return <ListContacts contacts={this.state.contacts} />;
   }
 }
 
