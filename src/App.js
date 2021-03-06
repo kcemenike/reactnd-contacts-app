8120d171c15d925af24ddadf5485e6639e70693d
kcemenike <29731161+kcemenike@users.noreply.github.com>
2021-03-06 20:57:27 +0100

    added Route feature with exact path match

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React, { Component } from "react";
 import CreateContact from "./CreateContact";
 import ListContacts from "./ListContacts";
 import * as ContactsAPI from "./utils/ContactsAPI";
+import { Route } from "react-router-dom";
 
 class App extends Component {
   state = {
     contacts: [],
-    screen: "list",
+    // screen: "list",
   };
   componentDidMount() {
     ContactsAPI.getAll().then((contacts) => {
@@ -29,18 +30,23 @@ class App extends Component {
   render() {
     return (
       <div>
-        {this.state.screen === "list" && (
-          <ListContacts
-            contacts={this.state.contacts}
-            onDeleteContact={this.removeContact}
-            onNavigate={() => {
-              this.setState(() => ({
-                screen: "create",
-              }));
-            }}
-          />
-        )}
-        {this.state.screen === "create" && <CreateContact />}
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <ListContacts
+              contacts={this.state.contacts}
+              onDeleteContact={this.removeContact}
+              // onNavigate={() => {
+              //   this.setState(() => ({
+              //     screen: "create",
+              //   }));
+              // }}
+            />
+          )}
+        />
+        {/* <Route path="/create" render={() => <CreateContact />} /> */}
+        <Route path="/create" component={CreateContact} />
       </div>
     );
   }
