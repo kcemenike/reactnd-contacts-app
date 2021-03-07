add9da8f757b31eb9440c5532cdeb3966c9a049d
kcemenike <29731161+kcemenike@users.noreply.github.com>
2021-03-07 12:06:11 +0100

    changes to src files to complete project

--- src/App.js
+++ src/App.js
@@ -27,6 +27,14 @@ class App extends Component {
     ContactsAPI.remove(contact);
   };
 
+  createContact = (contact) => {
+    ContactsAPI.create(contact).then((contact) => {
+      this.setState((currentState) => ({
+        contacts: currentState.contacts.concat([contact]),
+      }));
+    });
+  };
+
   render() {
     return (
       <div>
@@ -45,8 +53,18 @@ class App extends Component {
             />
           )}
         />
-        {/* <Route path="/create" render={() => <CreateContact />} /> */}
-        <Route path="/create" component={CreateContact} />
+        <Route
+          path="/create"
+          render={({ history }) => (
+            <CreateContact
+              onCreateContact={(contact) => {
+                this.createContact(contact);
+                history.push("/");
+              }}
+            />
+          )}
+        />
+        {/* <Route path="/create" component={CreateContact} /> */}
       </div>
     );
   }
